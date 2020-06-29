f0858b85c632731c7133bf0143e80b5c9047f21b
Nathan <znathangirotz@gmail.com>
2020-06-29 17:16:23 +0200

    commit tp plugin

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,10 +4,16 @@ package dev;
  * Hello world!
  *
  */
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class App 
-{
+{	  
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        String asciiArt = FigletFont.convertOneLine("Hello World!");
+	    System.out.println(asciiArt);
     }
 }
